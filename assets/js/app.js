226be47bd77d79dfbf42f2e10b6c888a2da83b49
Muhammad Fauzan <ojan14021@gmail.com>
2026-04-19 20:53:27 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+const form = document.getElementById("contactForm");
+const histori = document.getElementById("messageHistory");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nama = document.getElementById("name").value;
+  const pesan = document.getElementById("message").value;
+
+  // hapus tampilan kosong
+  const empty = histori.querySelector(".empty-state");
+  if (empty) {
+    empty.remove();
+  }
+
+  // waktu sekarang
+  const now = new Date();
+  const time = now.toLocaleString("id-ID", {
+    day:"numeric",
+    month:"short",
+    year:"numeric",
+    hour:"2-digit",
+    minute:"2-digit"
+  });
+
+  // buat elemen pesan
+  const item = document.createElement("div");
+  item.classList.add("message-item");
+
+  item.innerHTML = `
+    <div class="message-meta">
+      <span class="message-name">${nama}</span>
+      <span class="message-time">${time}</span>
+    </div>
+    <div class="message-content">"${pesan}"</div>
+  `;
+
+  histori.appendChild(item);
+
+  form.reset();
+});
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+window.addEventListener("scroll", () => {
+  navLinks.classList.remove("active");
+  hamburger.classList.remove("active");
+});
+
